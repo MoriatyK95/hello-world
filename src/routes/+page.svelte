f32ff96de5b2426ca75dd1b193ac508aaf15c0fc
MoriatyK95 <zenghuakai1995@gmail.com>
2026-02-02 21:28:37 +0800

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Header from './Header.svelte';
+	//set state with default value kai
+	let name = $state('kai');
+
+	let status: 'OPEN' | 'CLOSED' = $state('OPEN');
+
+    let full_name = $derived( name + " " + "zeng")
+</script>
+
+<Header {name} />
+<h2>{full_name}</h2>
+
+<input type="text" bind:value={name} />
+<p>The store is now {status}</p>
+<button
+	onclick={() => {
+		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
+	}}>Toggle Status</button
+>
+
+<style>
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts"> 
+    let { 
+        name 
+    } :{
+        name: string
+    } = $props();
+</script>
+
+<h1> Hello {name}</h1>
+<h1> {name.replaceAll('k', 't')}</h1>
+
+<style>
+
+</style>
